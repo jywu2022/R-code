473a58d8dd8c15e7ca81592001bad88a113477bc
jywu2022 <110119871+jywu2022@users.noreply.github.com>
2022-07-27 16:13:25 -0400

    Update github.R

--- github.R
+++ github.R
@@ -1,1 +1,7 @@
-
+anes<-read.csv(file="/Users/jiayiwu/Downloads/anes.csv",header=TRUE)
+d<-anes
+d$vote16new<-rep(NA,nrow(d))
+d$vote16new[d$vote16==1]<-"Trump"
+d$vote16new[d$vote16==2]<-"Clinton"
+d$vote16new[d$vote16>2]<-"Others"
+summary(d$vote16new)
